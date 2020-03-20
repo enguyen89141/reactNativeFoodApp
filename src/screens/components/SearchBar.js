f526266565a45279aa0a234f4b14ed526647f6ae
Eric Nguyen <enguyen89141@gmail.com>
2020-03-20 13:16:33 -0700

    first commit with basic search bar added

---
+++ src/screens/components/SearchBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return (
+        <View style={styles.background}>
+            <Feather name="search" size={30} />
+            <TextInput
+                placeholder="Search"
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 40,
+        borderRadius: 5,
+        marginHorizontal: 15,   
+        flexDirection: 'row'
+    }
+});
+
+export default SearchBar;
