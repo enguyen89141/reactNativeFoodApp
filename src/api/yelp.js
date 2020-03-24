e2c4781d0f399ed8623412fae7ccff5be4115d91
Eric Nguyen <enguyen89141@gmail.com>
2020-03-24 12:55:42 -0700

    extracted hook related logic

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businessess',
+    baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer sQpVsgXTHuwXoLkdqRCOtiyp22wPB0y4TivTP7D6ctJzzaUiU76jLlVYjJopZYc_5F8cxdqPCrab31SxvZ6PxrsYOD4d6GzH02T0c-0c-VLhL-6lwiZDHLzMvMlyXnYx'
+        Authorization: 
+            'Bearer sQpVsgXTHuwXoLkdqRCOtiyp22wPB0y4TivTP7D6ctJzzaUiU76jLlVYjJopZYc_5F8cxdqPCrab31SxvZ6PxrsYOD4d6GzH02T0c-0c-VLhL-6lwiZDHLzMvMlyXnYx'
     }
-})
+});
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,32 +1,19 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from './components/SearchBar';
-import yelp from '../api/yelp';
-
+import useResults from '../hooks/useResults';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('')
-    const [results, setResults] = useState([]);
-
-    const searchApi = async () => {
-       const response = await yelp.get('/search', {
-           params: {
-               limit: 50,
-               term,
-               location: 'las vegas'
-           }
-       });
-       setResults(response.data.businessess)
-    }
-
+    const [searchApi, results, errorMessage] = useResults();
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi()}
             />
-            <Text>Search Screen</Text>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     )
