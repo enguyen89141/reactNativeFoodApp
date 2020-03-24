eafe0c8e84c01d6b3bd5fa49f01dabb6aee2e7eb
Eric Nguyen <enguyen89141@gmail.com>
2020-03-24 13:37:32 -0700

    added list of results for restaurants with horizontal scrolling of name via flatlist

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+
     return (
-    <View style={styles.background}>
-        <Text>Search Bar</Text>
-    </View>
+        <View style={styles.background}>
+            <Feather
+                name="search"
+                size={30}
+                style={styles.iconStyle}
+            />
+            <TextInput
+                style={styles.inputStyle}
+                placeholder="Search"
+                value={term}
+                onChangeText={onTermChange}
+                autoCapitalize="none"
+                autoCorrect={false}
+                onEndEditing={onTermSubmit}
+            />
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     background: {
-        backgroundColor: '#FOEEEE'
+        backgroundColor: '#F0EEEE',
+        height: 40,
+        borderRadius: 5,
+        marginHorizontal: 15,
+        flexDirection: 'row',
+        marginTop: 10
+    },
+    inputStyle: {
+        fontSize: 18,
+        flex: 1
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 });
 
