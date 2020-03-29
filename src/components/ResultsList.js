8b8702c02081da1afbe1c950964620dccbe6bea7
Eric Nguyen <enguyen89141@gmail.com>
2020-03-28 17:44:47 -0700

    removed unnecessary naviation props and updated to use withNavigation

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, FlatList, TouchableOpacity} from 'react-native';
 import ResultsDetail from './ResultsDetail';
+import { withNavigation } from 'react-navigation';
 
 const ResultsList = ({ title, results, navigation}) => {
     return <View style={styles.container}>
@@ -39,4 +40,4 @@ const styles = StyleSheet.create({
     },
 });
 
-export default ResultsList;
+export default withNavigation(ResultsList);
